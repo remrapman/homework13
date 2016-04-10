40309772fe5c96805efd5d78269728a645e8ef84
Roman Boniuk <roma.boniuk@gmail.com>
2016-04-11 01:35:35 +0300

    Started HW, first commit

---
+++ homework13/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework13
+{
+    class Person
+    {
+        private const string InputFile = "Input.txt";
+        static List<string> input;
+        
+        
+
+        public static void CreatePerson()
+        {
+            input = new List<string>();
+            Program.ReadInput();
+            Random rand = new Random();
+            string NewPerson = new string(input[rand.Next(input.Capacity)]);
+            
+        }
+    }
+}
---
+++ homework13/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework13
+{
+    class Program
+    {
+        private const string InputFile = "Input.txt";
+        private const string WhiteListFile = "Whitelist.txt";
+        private const string BlackListFile = "Blacklist.txt";
+        private const string OutputBlack = "Output_blacklist.txt";
+        private const string OutputWhite = "Output_whitelist.txt";
+        private const string OutputBoth = "Output_black_and_white.txt";
+
+        static List<string> input;
+        static List<string> black;
+        static List<string> white;
+        static HashSet<string> forwritting;
+        static List<string> forfilter;
+        
+        static void Main(string[] args)
+    {
+        public static void ReadInput()
+        {
+            Readlines(InputFile, input);
+        }
+
+        private static void Readlines(string path, List<string> list)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    list.Add(line);
+                }
+            }
+        }
+
+        private static void Writelines(string outpath, List<string> list)
+        {
+            using (StreamWriter writer = new StreamWriter(outpath))
+            {
+                foreach (string line in list)
+                {
+                    writer.WriteLine(line);
+                }
+           }
+        }
+
+
+    }
+}
